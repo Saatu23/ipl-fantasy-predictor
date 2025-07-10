6b1a4ed478f2f5f8455e554f8fe0b9cbef703983
Saatu23 <satyamk.mishra.cd.mec23@itbhu.ac.in>
2025-07-10 07:26:59 +0530

    Ready for Deployment

--- app.py
+++ app.py
@@ -98,4 +98,6 @@ def download_csv():
     return send_file("data/predicted_best_11.csv", as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env var
+    app.run(host="0.0.0.0", port=port)
